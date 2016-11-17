3f795880280fc853d0a8b0b07961041c55fbe6b4
Rubén González Alonso <ruben.gonzalezalonso@telefonica.com>
2016-11-17 09:58:53 +0100

    Allow to configure visual baseline directory

--- toolium/test/test_config_files.py
+++ toolium/test/test_config_files.py
@@ -29,6 +29,7 @@ def config_files():
 def test_empty_config_files(config_files):
     assert config_files.config_directory is None
     assert config_files.output_directory is None
+    assert config_files.visual_baseline_directory is None
     assert config_files.config_properties_filenames is None
     assert config_files.config_log_filename is None
     assert config_files.output_log_filename is None
@@ -44,6 +45,12 @@ def test_set_output_directory(config_files):
     directory = '/tmp/fake'
     config_files.set_output_directory(directory)
     assert directory == config_files.output_directory
+
+
+def test_set_visual_baseline_directory(config_files):
+    directory = '/tmp/fake'
+    config_files.set_visual_baseline_directory(directory)
+    assert directory == config_files.visual_baseline_directory
 
 
 def test_set_config_properties_filenames(config_files):
