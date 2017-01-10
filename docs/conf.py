c45dc3cdb0ebdb2c23eda8db3c4d0e6d27a909b8
Rubén González Alonso <ruben.gonzalezalonso@telefonica.com>
2017-01-10 19:07:02 +0100

    Support Python 3.6

--- docs/conf.py
+++ docs/conf.py
@@ -70,7 +70,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Toolium'
-copyright = u'2015-2016, Telefónica I+D'
+copyright = u'2015-2017, Telefónica I+D'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
--- setup.py
+++ setup.py
@@ -67,6 +67,7 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Software Development :: Quality Assurance',
         'Topic :: Software Development :: Testing',
