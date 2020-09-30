9363eca3f74fdd9348e8e425eb050dc160b3a0b4
angel-ram <angel.ramosrodriguez@telefonica.com>
2020-09-30 11:55:37 +0200

    migrate utils downsload files

---
+++ toolium/test/utils/test_download_files.py
@@ -0,0 +1,142 @@
+# -*- coding: utf-8 -*-
+u"""
+Copyright 2018 Telefónica Investigación y Desarrollo, S.A.U.
+This file is part of Toolium.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+import urllib
+
+import mock
+import os
+import pytest
+
+try:
+    from urllib import urlretrieve, urlopen  # Py2
+except ImportError:
+    from urllib.request import urlretrieve, urlopen  # Py3
+
+from toolium.config_files import ConfigFiles
+from toolium.driver_wrapper import DriverWrapper
+from toolium.driver_wrappers_pool import DriverWrappersPool
+from toolium.utils.download_files import DOWNLOADS_FOLDER, get_download_directory_base, get_downloaded_file_path, \
+    retrieve_remote_downloaded_file
+
+
+@pytest.fixture
+def context():
+    # Create context mock
+    context = mock.MagicMock()
+    context.download_directory = ""
+
+    # Reset wrappers pool values
+    DriverWrappersPool._empty_pool()
+    DriverWrapper.config_properties_filenames = None
+
+    # Create a new wrapper
+    driver_wrapper = DriverWrappersPool.get_default_wrapper()
+    driver_wrapper.driver = mock.MagicMock()
+
+    # Configure properties
+    root_path = os.path.dirname(os.path.realpath(__file__))
+    config_files = ConfigFiles()
+    config_files.set_config_directory(os.path.join(root_path, 'conf'))
+    config_files.set_config_properties_filenames('properties.cfg')
+    config_files.set_output_directory(os.path.join(root_path, 'output'))
+    driver_wrapper.configure(config_files)
+    context.driver_wrapper = driver_wrapper
+
+    return context
+
+
+def test_get_download_directory_base_download_directory_none(context):
+    context.download_directory = None
+
+    assert get_download_directory_base(context) is None
+
+
+def test_get_download_directory_base_no_server_section(context):
+    context.download_directory = ""
+    context.driver_wrapper.config.remove_option('Server', 'enabled')
+
+    assert get_download_directory_base(context) == os.path.join(DriverWrappersPool.output_directory,
+                                                                DOWNLOADS_FOLDER, '')
+
+
+def test_get_download_directory_base_no_server_section_exception(context):
+    context.download_directory = ""
+    context.driver_wrapper.config.remove_option('Server', 'enabled')
+    DriverWrappersPool.output_directory = os.path.join(os.path.realpath(__file__), 'incorrect_path')
+
+    with pytest.raises(OSError):
+        get_download_directory_base(context)
+
+
+def test_get_download_directory_base_server_enabled_false(context):
+    context.download_directory = ""
+    context.driver_wrapper.config.set('Server', 'enabled', 'false')
+
+    assert get_download_directory_base(context) == os.path.join(DriverWrappersPool.output_directory,
+                                                                DOWNLOADS_FOLDER, '')
+
+
+def test_get_download_directory_base_server_enabled_true_no_driver_section(context):
+    context.download_directory = ""
+    context.driver_wrapper.config.set('Server', 'enabled', 'true')
+
+    assert get_download_directory_base(context) == "/tmp/%s/" % DOWNLOADS_FOLDER
+
+
+def test_get_download_directory_base_server_enabled_true_linux(context):
+    context.download_directory = ""
+    context.driver_wrapper.config.set('Server', 'enabled', 'true')
+    context.driver_wrapper.config.set('Driver', 'type', 'part1-part2-part3-linux')
+
+    assert get_download_directory_base(context) == "/tmp/%s/" % DOWNLOADS_FOLDER
+
+
+def test_get_download_directory_base_server_enabled_true_win(context):
+    context.download_directory = ""
+    context.driver_wrapper.config.set('Server', 'enabled', 'true')
+    context.driver_wrapper.config.set('Driver', 'type', 'part1-part2-part3-win')
+
+    assert get_download_directory_base(context) == 'C:\\tmp\\%s\\' % DOWNLOADS_FOLDER
+
+
+    ##########
+
+def test_get_downloaded_file_path_server_type_unknown(context):
+    context.driver_wrapper.server_type = 'unknown'
+    context.download_directory_base = '/'
+    context.download_directory = 'path'
+
+    assert get_downloaded_file_path(context, 'file_name') == '/path/file_name'
+
+
+    ##########
+
+
+def test_get_retrieve_remote_downloaded_file_download_directory_none(context):
+    context.download_directory = None
+
+    assert retrieve_remote_downloaded_file(context, 'file_name') is None
+
+
+def test_get_retrieve_remote_downloaded_file_download_no_server_section(context):
+    context.download_directory = 'donwload_path'
+    context.driver_wrapper.config.remove_option('Server', 'enabled')
+
+    assert retrieve_remote_downloaded_file(context, 'file_name') is None
+
+
+
