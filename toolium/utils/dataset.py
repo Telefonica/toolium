a53e5c3f487bc9e966d89dd5ddd32e992146a0e6
Patricia Roman Sanchez <patricia.romansanchez@telefonica.com>
2024-10-08 14:40:35 +0200

    Fix linter issues

--- toolium/utils/dataset.py
+++ toolium/utils/dataset.py
@@ -231,7 +231,7 @@ def _get_rounded_float_number(param):
     """
     type_mapping_regex = r'\[(ROUND):([\w\W]*)\]'
     type_mapping_match_group = re.match(type_mapping_regex, param)
-    if type_mapping_match_group and type_mapping_match_group.group(1) == 'ROUND':     
+    if type_mapping_match_group and type_mapping_match_group.group(1) == 'ROUND':
         replace_params = type_mapping_match_group.group(2).split('::')
         replace_param = f"{round(float(replace_params[0]), int(replace_params[1])):.{int(replace_params[1])}f}"
         return replace_param
