b03b631ef4a85a8a45c10f6dddda14510aabbdb0
Patricia Roman Sanchez <patricia.romansanchez@telefonica.com>
2024-10-08 12:30:45 +0200

    Fix tests issue

--- toolium/test/utils/test_dataset_replace_param.py
+++ toolium/test/utils/test_dataset_replace_param.py
@@ -377,9 +377,9 @@ def test_replace_param_dict():
 
 def test_replace_param_dict_json_format():
     param = replace_param('[DICT:{"key": "value", "key_2": true}]')
-    assert param == '{"key": "value", "key_2": True}'
+    assert param == {"key": "value", "key_2": True}
     param = replace_param('[DICT:{"key": "value", "key_2": null}]')
-    assert param == '{"key": "value", "key_2": null}'
+    assert param == {"key": "value", "key_2": None}
 
 
 def test_replace_param_upper():
